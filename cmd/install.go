7681c5d00a58051fc9abf3f21f5bb6d20127dd20
Jeremy Eu <jeu1993@gmail.com>
2022-02-14 23:36:29 +0800

    added crude method to list all installable versions

--- cmd/install.go
+++ cmd/install.go
@@ -7,10 +7,14 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"github.com/anaskhan96/soup"
 	"github.com/spf13/cobra"
+	"strings"
 	"svm/io"
 	"svm/parsers"
 )
+
+var listAllInstallable bool
 
 var installCmd = &cobra.Command{
 	Use:   "install",
@@ -21,6 +25,13 @@ Example:
 svm install 2.2.2-with-hadoop-2.7
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if listAllInstallable {
+			err := getAllInstallableVersions()
+			if err != nil {
+				return err
+			}
+			return nil
+		}
 		if err := validateArgs(args); err != nil {
 			return err
 		}
@@ -46,6 +57,38 @@ func validateArgs(args []string) error {
 	return nil
 }
 
+func getAllInstallableVersions() error {
+	const sparkURL = "https://archive.apache.org/dist/spark/"
+	fmt.Printf("Getting list of installable versions from %s\n", sparkURL)
+	response, err := soup.Get(sparkURL)
+	if err != nil {
+		return errors.New("cannot connect to https://archive.apache.org/dist/spark, check internet connection")
+	}
+	links := getSiteInfo(response)
+	for _, link := range links {
+		if strings.HasPrefix(link.Text(), "spark") {
+			subResponse, err := soup.Get(fmt.Sprintf("%s/%s", sparkURL, link.Text()))
+			if err != nil {
+				return errors.New("cannot connect to https://archive.apache.org/dist/spark, check internet connection")
+			}
+			subLinks := getSiteInfo(subResponse)
+			for _, subLink := range subLinks {
+				if strings.HasSuffix(subLink.Text(), ".tgz") {
+					println(parsers.ParseSparkFilename(subLink.Text()))
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func getSiteInfo(resp string) []soup.Root {
+	mainSite := soup.HTMLParse(resp)
+	links := mainSite.FindAll("a")
+	return links
+}
+
 func init() {
 	rootCmd.AddCommand(installCmd)
+	installCmd.Flags().BoolVarP(&listAllInstallable, "list", "l", false, "Lists all installable versions")
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/vbauerster/mpb/v7 v7.3.2
 )
 
+require github.com/anaskhan96/soup v1.2.5
+
 require (
 	github.com/VividCortex/ewma v1.2.0 // indirect
 	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
@@ -14,5 +16,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
